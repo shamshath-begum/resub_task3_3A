03432b5aaea55764f42bf92a2e0a4df5bfcdd9f2
shamshath begum <sham29.b@gmail.com>
2023-07-25 12:47:55 +0530

    first commit

---
+++ task3A/script.js
@@ -0,0 +1,27 @@
+let RESUME={
+    "NAME":"S SHAMSHATH BEGUM",
+    "ADDRESS":{"No":12,"STREET":"VALLIMAL","DISTRICT":"TRICHY","MOBILE NO":9842393430,"E-MAIL ID":"shama29.b@gmail.com"},
+    "isMARRIED":"TRUE",
+    "HOBBIES":"NULL",
+    "UG":"ECE",
+    "YEAR":2000,
+    "PERCENTAGE":"85%", 
+    "COLLEGE":"JCE",
+    "PG":"CSE",
+    "YEAR":2004,
+    "PERCENTAGE":"85%",
+    "COLLEGE":"KBN",
+    "EXPERIENCE":{
+        "TEACHING":{
+            "COLLEGE":{
+                "NAME":"KBN",
+                "YEARS":3,
+            },
+         TECHNICAL:{
+            "NAME":"AZAMTECHNOLOGIES",
+            "YEAR":1
+         }   
+        }
+    }
+}
+console.log(RESUME);
